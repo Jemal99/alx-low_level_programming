2cf962fcde1dd1d21c79424671e759dc391351f6
jemal <104770971+Jemal99@users.noreply.github.com>
2022-06-16 19:54:24 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN.H
+#define MAIN.H
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -13,4 +13,4 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 
-#endif
+#endif 
